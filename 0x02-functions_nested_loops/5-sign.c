cb9c9173b0b8109ddcbb59ac0aac6a4a33555fea
Zibonele <zibonelez47@gmail.com>
2022-09-14 10:20:32 -0700

    Getting used to it

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,15 +2,15 @@ include "main.h"
 
 /**
  * print_sign - prints the sign of a number
- *@num: number being tested
+ *@n: number being tested
  * Return: Always 0.
  */
 
-int print_sign(int num)
+int print_sign(int n)
 
 {
 
-if (num > 0)
+if (n > 0)
 
 {
 
@@ -20,7 +20,7 @@ return (1);
 
 }
 
-else if (num == 0)
+else if (n == 0)
 
 {
 
@@ -30,7 +30,7 @@ return (0);
 
 }
 
-else if (num < 0)
+else if (n < 0)
 
 {
 

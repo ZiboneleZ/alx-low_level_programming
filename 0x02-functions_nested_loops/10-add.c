a5156cb0237ac27dc30c5b7c4eefbf04df981985
Zibonele <zibonelez47@gmail.com>
2022-09-14 12:11:15 -0700

    Getting to know Mr Loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,15 @@
 
 /**
  * add - Add two integers
- * @x: int type number
- * @y: int type number
+ * @a: int type number
+ * @b: int type number
  * Return: result of addition
  */
 
-int add(int x, int y)
+int add(int a, int b)
 
 {
 
-return (x + y);
+return (a + b);
 
 }

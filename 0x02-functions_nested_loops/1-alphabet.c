45f8ba73ca31313a8c78da44cb36c8919429d907
Zibonele <zibonelez47@gmail.com>
2022-09-13 20:44:57 -0700

    Fixed the main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
   * Return: Always 0 (Success)
   */
 
-void print_alphabet(void)
+void print_alphabet(void) 
 
 {
 char alpha;
@@ -19,4 +19,6 @@ _putchar(alpha);
 
 _putchar('\n');
 
+return (0);
+ 
 }

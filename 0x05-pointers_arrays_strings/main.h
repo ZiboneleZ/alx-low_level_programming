edc9d2c48131c401a2ae51d4a721f77450d3f94c
Zibonele <zibonelez47@gmail.com>
2022-09-19 12:29:47 -0700

    Pointers and arrays commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,52 @@
+#ifndef INV_MAIN_H
+#define INV_MAIN_H
+
+#include <stdio.h>
+
+
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+size_t _strlen(const char *str);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+char *_strcpy(char *dest, char *src);
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif

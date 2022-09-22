e8d886334a9edac9aa161733e2b46a7c2de2e954
Zibonele <zibonelez47@gmail.com>
2022-09-21 17:07:08 -0700

    Newest commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,46 +1,40 @@
 #include "main.h"
 
 /**
- * _strchr - locate character in a string
- * @s: char array string
- * @c: char to look for
- * Return: NULL if char not found, or pointer to first occurrence of char `c`
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
 
 {
-  
-  int lengthD, lengthS;
-  
 
-  
-  lengthD = 0;
-  
-  lengthS = 0;
-  
+int lengthD, lengthS;
 
-  
-  while (*(dest + lengthD) != '\0')
-    
-    lengthD++;
-  
+lengthD = 0;
 
-  
-  while (*(src + lengthS) != '\0' && lengthD < 97)
-    
-    {
-      
-      *(dest + lengthD) = *(src + lengthS);
-      
-      lengthD++;
-      
-      lengthS++;
-      
-    }
-  
-  *(dest + lengthD) = '\0';
-  
-  return (dest);
-  
+lengthS = 0;
+
+while (*(dest + lengthD) != '\0')
+
+lengthD++;
+
+while (*(src + lengthS) != '\0' && lengthD < 97)
+
+{
+
+*(dest + lengthD) = *(src + lengthS);
+
+lengthD++;
+
+lengthS++;
+
+}
+
+*(dest + lengthD) = '\0';
+
+return (dest);
+
 }

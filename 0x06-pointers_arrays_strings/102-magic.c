3465373f50ecfab73e937c4883b10db4a0016818
Zibonele <zibonelez47@gmail.com>
2022-09-22 14:20:51 -0700

    Newest commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 
+
+
 int main(void)
-
+  
 {
+  
+  int n;
+  
+  int a[5];
+  
+  int *p;
+  
 
-int n;
-int a[5];
-int *p;
+  
+  a[2] = 1024;
+  
+  p = &n;
+  
+  /*
 
-a[2] = 1024;
-p = &n;
+   * write your line of code here...
 
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
+   * Remember:
 
-p[5] = 98;
+   * - you are not allowed to use a
 
-/* ...so that this prints 98\n */
+   * - you are not allowed to modify p
 
-printf("a[2] = %d\n", a[2]);
+   * - only one statement
 
-return (0);
+   * - you are not allowed to code anything else than this line of code
 
+   */
+  
+ p[5] = 98;
+  
+  /* ...so that this prints 98\n */
+  
+  printf("a[2] = %d\n", a[2]);
+  
+  return (0);
+  
 }

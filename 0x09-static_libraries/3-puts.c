b4e12d6c5e7c1ad0fb16f28ba7e0b7a9a69e4f55
Zibonele <zibonelez47@gmail.com>
2022-10-03 13:00:24 -0700

    New commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,27 @@
-void _puts(char *s);
+#include "main.h"
+
+/**
+ * _puts - prints an entire string.
+ * @str: tested string
+ * Return: always 0.
+ */
+
+void _puts(char *str)
+
+{
+
+int i;
+i = 0;
+while (*(str + i) != '\0')
+
+{
+
+_putchar(*(str + i));
+
+i++;
+
+}
+
+_putchar('\n');
+
+}

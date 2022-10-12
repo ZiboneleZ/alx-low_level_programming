f6efaf6af11b3fcfb5b80fd5f1ec3d764b6dcb51
Zibonele <zibonelez47@gmail.com>
2022-10-12 15:50:38 -0700

    New structure

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,51 +1,33 @@
-#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 
 /**
- * main - main function
- * @argc: argument count
- * @argv: string of arguments in array
- * Return: 0
+ * main - define function
+ * @argc: count of arguments
+ * @argv: pointer to array of pointers
+ * Return: 0 on success, 98 on malformed arguments, 99 if no operator
  */
 
 int main(int argc, char *argv[])
 
 {
 
-int a, b;
-
-int (*o)(int, int);
+int num1, num2;
 
 if (argc != 4)
-{
 
-printf("Error\n");
-exit(98);
+printf("Error\n"), exit(98);
 
-}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-if (argv[2][1] != '\0')
+if (get_op_func(argv[2]) == NULL)
+printf("Error\n"), exit(99);
 
-{
-
-printf("Error\n");
-exit(99);
-
-}
-
-o = get_op_func(argv[2]);
-
-if (o == NULL)
-
-{
-printf("Error\n");
-exit(99);
-}
-
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-printf("%d\n", o(a, b));
+if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
+printf("Error\n"), exit(100);
+printf("%d\n", (get_op_func(argv[2])(num1, num2)));
 
 return (0);
 

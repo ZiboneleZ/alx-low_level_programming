041f9780ac42554f5c1f4203d318467f49bfb47a
ZiboneleZ <zibonelez47@gmail.com>
2022-10-21 18:42:34 -0700

    Singly Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,7 @@
  * b - prints from init by the loader before main
  */
 
-void b(void) __attribute__ ((constructor));
-void b(void)
+void b(void) __attribute__ ((constructor)) before_main(void)
 
 {
 

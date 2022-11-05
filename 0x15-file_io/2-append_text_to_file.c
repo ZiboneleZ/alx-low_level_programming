3f3a4125fc5933ab28c4e9cb151a1f7463550e66
ZiboneleZ <zibonelez47@gmail.com>
2022-11-04 21:03:08 -0700

    New commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,18 +8,18 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int append_file, len, wr_stat;
+int append_file, len, wr_stat;
 
-  if (filename == NULL)
-    return (-1);
-  append_file = open(filename, O_WRONLY | O_APPEND);
-  if (append_file == -1)
-    return (-1);
-  if (text_content == NULL)
-    return (1);
-  for (len = 0; text_content[len]; len++)
-    ;
-  wr_stat = write(append_file, text_content, len);
-  close(append_file);
-  return (wr_stat == -1 ? -1 : 1);
+if (filename == NULL)
+return (-1);
+append_file = open(filename, O_WRONLY | O_APPEND);
+if (append_file == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+for (len = 0; text_content[len]; len++)
+;
+wr_stat = write(append_file, text_content, len);
+close(append_file);
+return (wr_stat == -1 ? -1 : 1);
 }

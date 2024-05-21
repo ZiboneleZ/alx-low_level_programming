a6ef56d6af1cc855467f04b3539978f1917a1e4c
ZiboneleZ <zibonelez47@gmail.com>
2024-05-21 20:36:04 +0200

    Done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,44 @@
 #include "search_algos.h"
 #include <math.h>
 
-int min_value(int num1, int num2);
-
 /**
- * jump_search - Searches value in array of ints using the Jump search algo
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
- * @array: Array to search
- *
- * @size: Size of the array
- *
- * @value: Value to search
- *
- * Return: First index where value is located or -1 for NULL array
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	/* Define jump number by square root of size */
-	int jump = sqrt(size);
-	int index = 0;
+	int index, m, k, prev;
 
-	/* Check inputs */
-	if (!array)
-	{
+	if (array == NULL || size == 0)
 		return (-1);
-	}
 
-	/* Find block of array where value is found in jump_step */
-	while (index < (int)size && array[index] < value)
-	{
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-		index += jump;
-	}
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-	/* Set jumpsize to index at beginning of array block to search*/
-	index -= jump;
-
-	printf("Value found between indexes [%d] and [%d]\n", index, index + jump);
-
-	/* Value found in block of size jump_step. Linearly find value */
-	while (index <= (index + jump) && index < (int)size)
-	{
+	do {
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
-		/* Found return value at index */
 		if (array[index] == value)
-		{
 			return (index);
-		}
-		index++;
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
+
 	return (-1);
 }
